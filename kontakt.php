15f85eabf740bc06b8fde7ae49fb1854cc890b28
Nostix <mail@nostix.de>
2017-06-28 16:18:26 +0200

    clean and comment kontakt.php and mail.php

--- kontakt.php
+++ kontakt.php
@@ -1,30 +1,44 @@
+<!--
+/********************************************************
+* Dateiname: kontakt.php
+* Autor: Nostix Code
+* letzte Aenderung: 28.06.2017
+* Inhalt: Kontaktformular
+*
+* Verwendete Funktionen (aus anderen Dateien):
+*   (window).scroll(function ()
+*   
+*
+* Definierte Funktionen:
+*   /
+********************************************************/
+-->
 <?php session_start(); ?>
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Kontakt</title>
-        <meta name="description" content="Trapfestival">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <title>Kontakt</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Trapfestival">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/main.css">
-        <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon" />
-		 <link rel="icon" href="img/logo.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon" />
+		<link rel="icon" href="img/logo.ico" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
 
-        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-
-        <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
-        <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
-        <script src="js/cookies.js"></script>
-    </head>
-    <body>
-      
-  <!--####### Navbar #######-->
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+    <script src="js/cookies.js"></script>
+  </head>
+  <body>
+  <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
+      <!-- Navigation für Mobile Geräte -->
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
@@ -34,66 +48,65 @@
           </button>
           <a class="navbar-brand" href="index.php">Claptrap</a>
         </div>
-    <div class="collapse navbar-collapse" id="navbar">
-      <ul class="nav navbar-nav">
+        <!-- Eigentliche Navigation -->
+        <div class="collapse navbar-collapse" id="navbar">
+          <ul class="nav navbar-nav">
             <li><a href="about.php">Band</a></li>
             <li><a href="anfahrt.php">Anfahrt</a></li>
             <li><a class="active" href="kontakt.php">Kontakt</a></li>
             <li><a href="guestbook.php">G&auml;stebuch</a></li>
           </ul>
-      </div>
+        </div>
       </div>
     </nav>
-    <div style="float: right;">
-    <img style="position: relative;" src="img/overlaybesucherzahler.png">
-    <?php include ("counter.php"); ?>
+    <!-- Besucherzähler -->
+    <div class="Besucherzaehler">
+      <img class="Besucherhintergrund" src="img/overlaybesucherzahler.png">
+      <?php include ("counter.php"); ?>
     </div>
-    
     <div class="container">
-    <div class="panel panel-transparent">
-      <div class="panel-heading">
-        <h3>Kontaktformular</h3>
+      <div class="panel panel-transparent">
+        <div class="panel-heading">
+          <h3>Kontaktformular</h3>
+        </div>
+        <div class="panel-body">
+          <?php
+            // Benachrichtignung nach erfolgreichem Senden
+            if(isset($_GET['mailsent']))
+            {
+              echo '<div class="alert alert-success" role="alert"><strong>Danke!</strong> Ihre nachricht wurde gesendet.</div>';
+            }
+          ?>
+          <!-- Kontaktformular -->
+          <form action="mail.php" method="post">
+            <div class="form-group loginform">
+              <br />
+              <div class="ResponsiveFormularLinks">
+                <label for="Name">Name*</label>
+                <input type="text" class="form-control" name="Name" id="Name" maxlength="40" placeholder="Name" required><br>
+                <label for="Email">Email*</label>
+                <input type="email" class="form-control" name="Email" id="Email" maxlength="70" placeholder="Email-Adresse" required><br>
+              </div>
+              <div class="ResponsiveFormularRechts">
+                <label for="Nachricht">Nachricht*</label>
+                <textarea class="form-control" name="Nachricht" id="Nachricht" rows="5" maxlength="800" placeholder='Schreiben Sie hier ihre Nachricht.' required></textarea><br>
+                <button type="submit" class="btn btn-success">Senden</button>
+              </div>
+            </div>
+          </form>
+          *Erforderliche Felder
+        </div>
       </div>
-      <div class="panel-body">
-        <?php
-          if(isset($_GET['mailsent'])) {
-            echo '<div class="alert alert-success" role="alert"><strong>Danke!</strong> Ihre nachricht wurde gesendet.</div>';
-          }
-        ?>
-        <form action="mail.php" method="post">
-          <div class="form-group loginform">
-          <br>
-          <div class="formfirst">
-            <label for="name">Name*</label>
-            <input type="text" class="form-control" name="name" id="name" maxlength="40" placeholder="Name" required><br>
-            <label for="email">Email*</label>
-            <input type="email" class="form-control" name="email" id="email" maxlength="70" placeholder="Email-Adresse" required><br>
-          </div>
-          <div class="formsecond">
-            <label for="nachricht">Nachricht*</label>
-            <textarea class="form-control" name="nachricht" id="nachricht" rows="5" maxlength="800" placeholder='Schreiben Sie hier ihre Nachricht.' required></textarea><br>
-            <button type="submit" class="btn btn-success">Senden</button>
-          </div>
-          </div>
-        </form>
-        *Erforderliche Felder
-      </div>
-    </div>
-
-  <!--####### Back-to-top #######-->
-   <a id="back-to-top" href="#" class="btn btn-default btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
-
-  <!--####### Footer #######-->
-   <div class="container">
+      <!-- Back to Top Button -->
+      <a id="back-to-top" href="#" class="btn btn-default btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+      <div class="container">
       <hr>
-
       <footer>
         <p>&copy; Nostix Code 2k17 | <a href="impressum.php">Impressum</a> | <a href="admin.php">Admin-Bereich</a></p>
       </footer>
-    </div> <!-- /container -->
-        <script src="js/vendor/jquery-1.11.2.min.js"></script>
-        <script src="js/vendor/bootstrap.min.js"></script>
-        <script src="js/main.js"></script>
-   </div>
-    </body>
+    </div>
+    <script src="js/vendor/jquery-1.11.2.min.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
 </html>
--- mail.php
+++ mail.php
@@ -1,12 +1,30 @@
+<!--
+/********************************************************
+* Dateiname: mail.php
+* Autor: Nostix Code
+* letzte Aenderung: 28.06.2017
+* Inhalt: Kontakt mail senden
+*
+* Verwendete Funktionen (aus anderen Dateien):
+*   /
+*   
+*
+* Definierte Funktionen:
+*   /
+********************************************************/
+-->
 <?php
-$myemail = 'mail@nostix.de';
-$name = $_POST['name'];
-$email_adresse = $_POST['email'];
-$nachricht = $_POST['nachricht'];
-$an = $myemail;
-$betreff = 'Nachricht von Kontaktformular: '.$name;
-$nachricht = "Es wurde eine Nachricht gesendet von: ".$name."\r\n \r\nEmail: ".$email_adresse."\r\n \r\nNachricht: \r\n".$nachricht;
-mail($an, $betreff, $nachricht);
-sleep(1);
-header("Location:kontakt.php?mailsent");
+  // Variablen
+  $MeineEmail = 'mail@nostix.de';
+  $Name = $_POST['Name'];
+  $NutzerEmail = $_POST['Email'];
+  $Nachricht = $_POST['Nachricht'];
+  $An = $myemail;
+  $Betreff = 'Nachricht von Kontaktformular: '.$Name;
+  $EmailNachricht = "Es wurde eine Nachricht gesendet von: ".$Name."\r\n \r\nEmail: ".$NutzerEmail."\r\n \r\nNachricht: \r\n".$Nachricht;
+  // Mail senden
+  mail($An, $Betreff, $EmailNachricht);
+  sleep(1);
+  // Erfolg, Umleiten
+  header("Location:kontakt.php?mailsent");
 ?>
